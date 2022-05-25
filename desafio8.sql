2ad55d2e29f7cd961c1678dc60ddb521c8864f57
Bruna <brunac.cardososilva@gmail.com>
2022-05-25 14:40:25 -0300

    Adicionado todos os arquivos para testes
    Em desenvolvimento
    
    Ultimo resolvido req 18.

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+CREATE TABLE A (
+  name INTEGER
+);
+CREATE TABLE Trybe (
+  name VARCHAR(40)
+);
+CREATE TABLE eh (
+  name INTEGER
+);
+INSERT INTO A (name)
+  VALUES (5 + 6);
+INSERT INTO Trybe (name)
+  VALUES ('de');
+INSERT INTO eh (name)
+  VALUES (2 + 8);
