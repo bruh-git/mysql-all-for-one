968b5d576f7af7100d63f0ae426f2c629be2f454
Bruna <brunac.cardososilva@gmail.com>
2022-05-25 16:16:47 -0300

    Ajustes em req com erro.

--- desafio22.sql
+++ desafio22.sql
@@ -1,4 +1,3 @@
-SELECT discount FROM northwind.order_details;
 UPDATE northwind.order_details
 SET discount = '15'
 WHERE discount ='0'; 
--- desafio25.sql
+++ desafio25.sql
@@ -1,3 +1,2 @@
-SELECT unit_price FROM northwind.order_details;
 DELETE FROM northwind.order_details
 WHERE unit_price <'10.0000';
