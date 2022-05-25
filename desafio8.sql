19e184c8935554c800d45fa179a725050d131bbe
Bruna <brunac.cardososilva@gmail.com>
2022-05-25 16:38:37 -0300

    Ajustes nos requisitos 5,8,10,15.

--- desafio8.sql
+++ desafio8.sql
@@ -1,15 +1,1 @@
-CREATE TABLE A (
-  name INTEGER
-);
-CREATE TABLE Trybe (
-  name VARCHAR(40)
-);
-CREATE TABLE eh (
-  name INTEGER
-);
-INSERT INTO A (name)
-  VALUES (5 + 6);
-INSERT INTO Trybe (name)
-  VALUES ('de');
-INSERT INTO eh (name)
-  VALUES (2 + 8);
+SELECT 5+6 AS 'A', 'de' AS 'Trybe', 2+8 AS 'eh';
